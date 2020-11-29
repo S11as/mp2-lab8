b0c583eb2b2656ad1e6af6df0245f8960a72245f
S11as <50956430+S11as@users.noreply.github.com>
2020-11-29 12:30:47 +0300

    small fix

--- stacklist/main.cpp
+++ stacklist/main.cpp
@@ -3,13 +3,5 @@
 
 int main()
 {
-    TStackList<int> s;
-    for (int i = 0; i < 20; ++i) {
-        s.push(i);
-    }
-    s.save("tmp.txt");
-
-    TStackList<int> b = TStackList<int>::load("tmp.txt");
-
     return 0;
 }
